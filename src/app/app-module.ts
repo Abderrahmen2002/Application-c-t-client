c7e245a1128f3e4d264674947f7e279db272f4c4
Abderrahmen2002 <abderrahmen.nasri@esprit.tn>
2025-10-01 11:38:17 +0100

    wassup

---
+++ src/app/app-module.ts
@@ -0,0 +1,30 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Footer } from './layout/footer/footer';
+import { Header } from './layout/header/header';
+import { Notfound } from './layout/notfound/notfound';
+import { Home } from './layout/home/home';
+import { ListEvent } from './layout/list-event/list-event';
+
+@NgModule({
+  declarations: [
+    App,
+    Footer,
+    Header,
+    Notfound,
+    Home,
+    ListEvent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
---
+++ src/app/app-routing-module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './layout/home/home';
+import { Notfound } from './layout/notfound/notfound';
+import { ListEvent } from './layout/list-event/list-event';
+
+const routes: Routes = [
+  {path: 'home', component: Home},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', component: Notfound},
+  {path: 'list', component: ListEvent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
